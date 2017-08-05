8d38e54e15d37f9812b92db1e77fb0e6e5be2136
Happy.Shandilya <happyshandilya@gmail.com>
2017-08-05 12:19:44 -0600

    filter and map method  using callback

--- src/app/call-back-component/call-back-component.component.ts
+++ src/app/call-back-component/call-back-component.component.ts
@@ -21,6 +21,23 @@ export class CallBackComponentComponent implements OnInit {
     var d = this.calculate(10, 5, (x, y) => { return x - y })
     console.log("alright arrow function " + d);
 
+    // some exampls of callback method
+
+    //map()
+    var array = [1, 2, 3, 4, 5];
+    var newArray = array.map(
+      (x) => { return x * x }
+    );
+    console.log("aha newArray " + newArray);
+    // filter()
+
+    var array1 = [1, 2, 3, 4, 5];
+
+    function isOdd(x) {
+      return x % 2 != 0;
+    }
+    var newArray = array1.filter(isOdd);
+    console.log("after going through filter callback " + newArray);
   }
   //Callback functions are functions that are passed as arguments into other functions to be executed at a later point in time.
   mul(x, y) {
