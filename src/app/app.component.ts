35160b7f375aa78aa80444d046470cafd80f76bf
Happy.Shandilya <happyshandilya@gmail.com>
2017-08-05 11:25:57 -0600

    Async Vs Sync

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 
-  constructor(){
- this.funcA();
+  constructor() {
+    this.funcA();
+    this.fastTask();
+    this.slowTask();
+    this.fastTask();
+    this.fastTask();
+
+    // calling async
+    this.superFastTask();
+    this.asyncLazySlowTask();
+    this.superFastTask();
+    this.asyncLazySlowTask();
+    this.superFastTask();
+    this.superFastTask();
   }
   // the call stack
   funcA() {
@@ -29,6 +41,32 @@ export class AppComponent {
     console.log("now in function C")
     console.log(Error().stack);
   }
+  // Synchronousvs Asynchronous Programming
+  //here we see how slowTask() takes a long time to process and prevents other fast tasks from processing
+  slowTask() {
+    //it takes 3 seconds to process
+    var now = new Date().getTime();
+    while (new Date().getTime() < now + 3000) {
+      console.log("It's processing here")
+    }
+    console.log("slow task finished")
+  }
 
+  fastTask() {
+    console.log("fast task finished");
+  }
 
+  //Asyncronous will preventbloacking of other task because of the lazyslowtask
+
+  lazySlowTask() {
+    console.log("slow task finished here in laziness");
+  }
+
+  asyncLazySlowTask() {
+    setTimeout(this.lazySlowTask, 4000);// this task finishes in 4 seconds, but is processed on a seperate thread alright
+  }
+
+  superFastTask() {
+    console.log("aha super fast task finished");
+  }
 }
