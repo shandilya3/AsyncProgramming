61cf560bcbc416952f32187db49cdb362aa59c2b
Happy.Shandilya <happyshandilya@gmail.com>
2017-08-05 11:03:33 -0600

    call Stack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(){
+ this.funcA();
+  }
+  // the call stack
+  funcA() {
+    console.log("Starting in Function A")
+    this.funcB();
+    console.log("Waiting for funcB execution")
+
+  }
+
+
+  funcB() {
+    console.log("Starting in Function B")
+    this.funcC();
+  }
+
+  funcC() {
+    console.log("now in function C")
+    console.log(Error().stack);
+  }
+
+
 }
