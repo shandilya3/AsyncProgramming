69b4c4bcb404d31391c23910f850b6ab64db7516
Happy.Shandilya <happyshandilya@gmail.com>
2017-08-06 11:44:25 -0600

    basics of Generator function and Yield keyword

--- src/app/generator-function/generator-function.component.ts
+++ src/app/generator-function/generator-function.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneratorFunctionComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+//the fuction* keyword is used to declare THE generator funtion
+
+// the yield keyword is used to pause the generator and may also be used to receive an input and send the otuput from the generator
+
+
+    function* genFunc() {
+
+      yield 'a';   //pauses thegenerator and sends out a value of 'a'
+      yield;       // pauses the generator and sends out an undefined value
+      yield 123;   // pauses the generator and sends out a value of 123
+
+      return "finished";   // the return value of the Generator function is often left unused
+
+    }
+
+
+
+
+
+
+  }
 
   ngOnInit() {
   }
